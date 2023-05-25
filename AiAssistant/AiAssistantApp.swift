691e590699f91999e973bbc44b2d9a4cde9718d6
Danny Sandy <dsandy67@haverfordsd.net>
2023-05-25 10:57:36 -0400

    Initial Commit

---
+++ AiAssistant/AiAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiAssistantApp.swift
+//  AiAssistant
+//
+//  Created by  on 5/25/23.
+//
+
+import SwiftUI
+
+@main
+struct AiAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
